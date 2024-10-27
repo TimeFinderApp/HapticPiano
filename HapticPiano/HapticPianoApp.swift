d94d855c9e9ab0e165fa1dfa0a5975176b91919f
Luke Memet <lukememet@gmail.com>
2024-10-27 15:05:32 -0400

    Initial Commit

---
+++ HapticPiano/HapticPianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticPianoApp.swift
+//  HapticPiano
+//
+//  Created by Luke Memet on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct HapticPianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
